43154d34e90dfe364c5379eb10b49ad13350dd66
Victor LT <victor_lamas@hotmail.com>
2024-12-31 14:21:00 +0100

    Actividad 1.2.1 FINAL STABLE

--- app/src/main/java/edu/actividad/demo06/CityApplication.kt
+++ app/src/main/java/edu/actividad/demo06/CityApplication.kt
@@ -4,6 +4,12 @@ import android.app.Application
 import androidx.room.Room
 import edu.actividad.demo06.data.CitiesRoomDB
 
+/**
+ * Initialize the database.
+ *
+ * @property cityDB The database of the cities.
+ * @author Víctor Lamas
+ */
 class CityApplication : Application() {
     lateinit var cityDB: CitiesRoomDB
         private set
